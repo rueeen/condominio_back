c95fe56c9216565380c68742bdc1bd9449ec0c7b
Ruben Valencia Arancibia <rvalencia@inacap.cl>
2026-08-14 16:33:16 -0400

    feat: wsgi changes

--- condominio_back/wsgi.py
+++ condominio_back/wsgi.py
@@ -7,9 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
+import os
+os.environ["OMP_THREAD_LIMIT"] = "1"
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'condominio_back.settings')
 
